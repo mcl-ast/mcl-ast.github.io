9ce0306b56f9d32efe40208f9c218be0cbca08d3
Joel Kraft <joelkraft3@yahoo.com>
2014-03-14 23:35:24 -0700

    complete work on latest stable version of gather

--- gather-browser-button.beta.js
+++ gather-browser-button.beta.js
@@ -1,5 +1,5 @@
 javascript:(function() {
-	var url = 'http://mcl-ast.github.io/gather.beta.js',
+	var url = 'http://localhost/~joelkraft/Jobs/Multco/retrieval_upgrade_nov-2013/bookmarklet.js',
 		noCache = '?nc=' + (parseInt(Math.random() * 100000000000)).toString(),
 		head = document.getElementsByTagName('head')[0],
 		createAndAppendScriptTag = function createAndAppendScriptTag(attrs) {
@@ -12,7 +12,8 @@ javascript:(function() {
 			return script;
 		},
 		deleteGlobal = function deleteGlobal() {
-			delete window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT;
+			delete window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT
+;
 		},
 		i = 1;	
 
@@ -77,5 +78,7 @@ javascript:(function() {
 	createAndAppendScriptTag({'src':(url + noCache),'type':'text\/javascript', 'onerror':loadError});
 	requireExternalScript(useLocalCopyOfScript());
 })()
+
+=============
 
 javascript:(function(){function o(){if(window.localStorage&&window.localStorage.retrievalSelectionScript){var e=JSON.parse(window.localStorage.retrievalSelectionScript).scriptString;r({type:"text/javascript",text:e,onerror:u});return true}return false}function u(e){console.log("Remote Script could not be loaded.");if(e===true){console.log("Remote copy of script was unavailable.  Local copy may be out of date.")}else{alert("This function is not available at this time.  Please ensure you are on a Catalog page, and try again.");console.log("window.localStorage: "+!!window.localStorage+"\nwindow.localStorage.retrievalSelectionScript: "+!!window.localStorage.retrievalSelectionScript);i();throw new URIError("The script "+e.target.src+" is not accessible.")}return true}function a(){if(window.localStorage){if(window.localStorage.retrievalSelectionScript){var e=JSON.parse(window.localStorage.retrievalSelectionScript).version;if(e===window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT.version){i();return}}window.localStorage.retrievalSelectionScript=JSON.stringify(window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT)}i()}function f(e){if(s>60){u(e);return}if(window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT===undefined){console.log("Timeout was called "+s++ +" times.");setTimeout(f,4,e);return}else{console.log("success");if(!e){r({text:window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT.scriptString,type:"text/javascript",onerror:u})}a()}}var e="http://mcl-ast.github.io/gather.beta.js",t="?nc="+parseInt(Math.random()*1e11).toString(),n=document.getElementsByTagName("head")[0],r=function(t){var r=document.createElement("script"),i;for(i in t){if(t.hasOwnProperty(i))r[i]=t[i]}n.appendChild(r);return r},i=function(){delete window.GLOBAL_RETRIEVAL_BOOKMARKLET_SCRIPT},s=1;i();r({src:e+t,type:"text/javascript",onerror:u});f(o())})()
